4ec8d279285340a6602d5fc852cec893b54e5edd
yousefHelly <yousef.helly@gmail.com>
2024-03-09 17:11:44 +0200

    updating astro config to deploy it on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://yousefhelly.github.io',
+  base:'/PRODIGY_WD_01',
   integrations: [tailwind(), react()]
 });
